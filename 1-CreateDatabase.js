a22f4aa57fe7fceb1f6471505e3e5088f7b24972
obddd <obaidulhaq20@gmail.com>
2021-03-06 17:59:29 +0500

    config dotenv

--- 1-CreateDatabase.js
+++ 1-CreateDatabase.js
@@ -1,9 +1,10 @@
 const faunadb = require('faunadb');
 const q = faunadb.query;
+require('dotenv').config();
 
 (async () => {
   const client = new faunadb.Client({
-    secret: 'fnAEDoH5ooACAZDHaYjpx48haqa1dpvS3VL_4VtZ',
+    secret: process.env.FAUNADB_SERVER_SECRET,
   });
   try {
     const result = await client.query(
